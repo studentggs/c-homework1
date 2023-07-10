4c38062efe163bb1644da406353533b9085f8540
gor <student.ggs@mail.ru>
2023-07-10 17:55:35 +0400

    ДЗ

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿// Домашнее задание по Семинару 1.
+
+// Задача 1
+
+/*
+Console.WriteLine("first number: ");
+int firstN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("second number: ");
+int secondN = Convert.ToInt32(Console.ReadLine());
+if (firstN > secondN )
+{
+    Console.WriteLine($"max = {firstN}, min = {secondN}");
+
+}
+else
+{
+    Console.WriteLine($"max = {secondN}, min = {firstN}");
+}
+*/ 
+
+
+// Задача 2 
+/*
+Console.WriteLine("first number: " );
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("trible number: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if (n1>n2)
+{
+if (n1>n3)
+    {
+        Console.WriteLine($"max = {n1}");
+    }
+    else 
+    {
+        Console.WriteLine($"max = {n3}");
+    }
+}
+else
+if (n2>n3)
+{
+    Console.WriteLine($"max = {n2}");
+}
+else
+{
+    Console.WriteLine($"max = {n3}");
+}
+*/
+
+// Задача 3
+
+/*
+Console.WriteLine("Input number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber %2 == 0) 
+{
+    Console.WriteLine($"yes");
+}
+else
+{
+    Console.WriteLine($"no");
+}
+*/
+
+// Задача 4
+
+
+Console.WriteLine("Input number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+ // c Четвертым никак не получается 
